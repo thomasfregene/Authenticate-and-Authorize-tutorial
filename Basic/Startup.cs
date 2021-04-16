675d4a1a1e23144217f3874a735b2f561260811c
thomasfregene <fregzythomas@gmail.com>
2021-04-16 20:35:11 +0100

    configuration at startup.cs

--- Basic/Startup.cs
+++ Basic/Startup.cs
@@ -16,6 +16,7 @@ namespace Basic
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,6 +31,7 @@ namespace Basic
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapDefaultControllerRoute();
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
