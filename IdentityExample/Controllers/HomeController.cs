37dc5515b814cea5193841019a939dbc17d3c33e
thomasfregene <fregzythomas@gmail.com>
2021-04-17 14:55:20 +0100

    Authentication Controller

---
+++ IdentityExample/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize] //attribute use to guard an action "are you allow to come here"
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Login(string username, string password)
+        {
+           
+            return RedirectToAction("Index");
+        }
+    }
+}
